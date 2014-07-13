5ab45cb1887879ec8e477439c37e3b6827220f45
littlemouseyuki <lisa@new-host-2.home>
2014-07-12 19:49:39 -0700

    reorganized.

---
+++ homework/my-own-commands/create-syscall.c
@@ -0,0 +1,10 @@
+//#include <syscall.h> 
+//#include <linux/types.h>
+#include <unistd.h>
+#include <stdio.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]) {
+	syscall(8, 0, argv[0], S_IRWXU);
+}
