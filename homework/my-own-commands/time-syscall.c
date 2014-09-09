3dc177212075c3e2a505f90a320f9ca2ad3602a6
Lisa <lrosenba@LisaUbuntu.(none)>
2014-09-09 16:27:24 -0700

    finished kernel and shell.

--- homework/my-own-commands/time-syscall.c
+++ homework/my-own-commands/time-syscall.c
@@ -4,15 +4,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <math.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
 
 int main(int argc, char* argv[]) {
-    int secs = syscall(13);
-    double mins = floor(secs/60);
-    double hours = floor(mins/60);
-    int minutes = (int)mins % 60;
-    char *c = ":";
-    syscall(4, 0, hours);
-    syscall(4, 0, c, strlen(c));
-    syscall(4, 0, minutes);
+    long secs;
+    struct tm *time;
+    char buff[80];
+    int ret = syscall(13, &secs);
+    time = localtime(&secs);
+    strftime(buff, 80, "%x - %I:%M%p %Z", time);
+    printf("%s\n", buff);
+    if (ret == -1) {
+           perror("syscall 13");
+    }
 }
 
